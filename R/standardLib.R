a09444e35a27c8da0d688d1d6f593235a86c3d86
mSparks43 <mark.parker01@googlemail.com>
2024-03-13 19:10:45 +0100

    initial fixes and graphs

--- R/standardLib.R
+++ R/standardLib.R
@@ -3,11 +3,25 @@
 # Zem Solutions, all rights reserved.
 # use `roxygen2::roxygenise()` to build
 
+## base Functions
+
+#' get standard folder names for current working directory
+#' @export
+folderInit <- function(){
+  baseDir <<- getwd()
+  dbDir<<- "../databases"
+  graphDir<<-"graphs/"
+  graphSubDir<<-""
+  outputDir<<-"output/"
+  dir.create(file.path(baseDir, graphDir), showWarnings = FALSE)
+  dir.create(file.path(baseDir, outputDir), showWarnings = FALSE)
+}
+
 ## Database functions
 
 #' import an NHM database into R
 #'
-#' @param dbName the number of workers
+#' @param dbName the ID of the patient database
 #' @return a dataframe containing patient record data in json
 #'
 #' @export
@@ -209,6 +223,42 @@ ci95_to_sd<-function(n,l95,u95){
   return(se_to_sd(se_local,n))
 }
 
+  rgbeta <- function(n=1000, mean=0.6, var=0.2, min = -0.6125, max = 1, a=NULL, b=NULL)
+  {
+    dmin <- mean - min
+    dmax <- max - mean
+
+    if (dmin <= 0 || dmax <= 0)
+    {
+      stop(paste("mean must be between min =", min, "and max =", max))
+    }
+
+    if (var >= dmin * dmax)
+    {
+      stop(paste("var must be less than (mean - min) * (max - mean) =", dmin * dmax))
+    }
+
+    # mean and variance of the standard beta distributed variable
+    mx <- (mean - min) / (max - min)
+    vx <- var / (max - min)^2
+
+    # find the corresponding alpha-beta parameterization
+    #a <- 20
+    #b <- 2
+    if(is.null(a))
+      a <- ((1 - mx) / vx - 1 / mx) * mx^2
+    if(is.null(b))
+      b <- a * (1 / mx - 1)
+
+    print(paste("Alpha parameter:", a))
+    print(paste("Beta parameter:", b))
+
+    # generate standard beta observations and transform
+    x <- rbeta(n, a, b)
+    y <- (max - min) * x + min
+
+    return(y)
+  }
 ##Graph helpers
 
 human_numbers <- function(x = NULL, smbl ="", signif = 1){
@@ -269,3 +319,51 @@ human_num   <- function(x){human_numbers(x, smbl = "")}
 #'
 #' @export
 human_rsd  <- function(x){human_numbers(x, smbl = "RSD ")}
+
+#' Create a plot with multiple lines
+#' @examples
+#' d<-data.frame()
+#' d<-rbind(d,data.frame(age=c(18:100),Legend="male",weight=rnorm(83,100,10)))
+#' d<-rbind(d,data.frame(age=c(18:100),Legend="female",weight=rnorm(83,80,5)))
+#' multilinePlot(d,"Random Weight Graph","Age","Weight(kg)","age","weight")
+#'
+#' @export
+multilinePlot <- function(dataSrc,t_title,xlabel,ylabel,xdataName,ydataName,yUnits="",seqB=0,seqE=110,seqBy=10){
+  ggplot(data = dataSrc, aes(x={{xdataName}}, y = {{ydataName}},color=Legend, linetype = Legend)) +
+    geom_line(linewidth=2) +
+    scale_x_continuous(breaks = seq(seqB, seqE, by = seqBy)) +
+    scale_color_manual(values=c("#6D9DC5","#80DED9","#068D9D","cyan","#53599A")) +
+    scale_y_continuous(labels = human_num) +
+    labs(x=xlabel, y=ylabel, title=t_title) +
+    theme(text = element_text(size = 20))
+}
+
+#' create a barchartPlot
+#'
+#' @param dataSrc dataframe data source
+#' @param t_title graph title
+#' @param xlabel graph x axis label
+#' @param ylabel graph y axis label
+#' @param xdataName column name to use for the X axis
+#' @param ydataName column name to use for the T axis
+#' @return a dataframe containing patient record data in json
+#' @export
+barchartPlot <- function(dataSrc,t_title,xlabel,ylabel,xdataName,ydataName){
+  ggplot(data= dataSrc, aes(x={{xdataName}}, y={{ydataName}}, fill=Legend)) +
+    geom_bar(stat='identity', position='dodge') +
+    theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))+
+    labs(x=xlabel, y=ylabel, title=t_title) +
+    scale_y_continuous(labels = human_numbers)
+}
+
+#' @export
+#'
+writePlot <- function(fileName,plotFunction){
+  folderInit()
+  setwd(graphDir)
+  savename<-paste(fileName,".png")
+  png(savename,width=300,height=150, pointsize=20, units = "mm", res = 300)
+  print(plotFunction)
+  dev.off()
+  setwd(baseDir)
+}

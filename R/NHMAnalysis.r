31c2d7ffe36ab72c8f090254dada2b6183e6381b
mSparks43 <mark.parker01@googlemail.com>
2024-04-03 22:39:09 +0200

    waterfall graph

--- R/NHMAnalysis.r
+++ R/NHMAnalysis.r
@@ -502,3 +502,135 @@ populationPyramid<-function(data){
     )
   return(population_pyramid)
 }
+
+#' @export
+#' https://stackoverflow.com/questions/48259930/how-to-create-a-stacked-waterfall-chart-in-r
+#'
+waterFallGraph<-function(data,graphTitle,yTitle,xaxis,category,valueField,threshold=80000000){
+  df <-
+    data.frame(
+      x.axis.Var = data[,xaxis][[1]],
+      cat.Var = data[,category][[1]],
+      values = data[,valueField][[1]])
+  tKey<-data[,category][[1]][1]
+  df.tmp <- df %>%
+    # \_Set the factor levels in the order you want ----
+  mutate(
+    x.axis.Var = factor(x.axis.Var,
+                        levels = unique(x.axis.Var)),
+    cat.Var = factor(cat.Var,
+                     levels = unique(cat.Var))
+  ) %>%
+    # \_Sort by Group and Category ----
+  arrange(x.axis.Var, desc(cat.Var)) %>%
+    # \_Get the start and end points of the bars ----
+  mutate(end.Bar = cumsum(values),
+         start.Bar = c(0, head(end.Bar, -1))) %>%
+    # \_Add a new Group called 'Total' with total by category ----
+  rbind(
+    df %>%
+      # \___Sum by Categories ----
+    group_by(cat.Var) %>%
+      summarise(values = sum(values)) %>%
+      # \___Create new Group: 'Total' ----
+    mutate(
+      x.axis.Var = "Ukupno",
+      cat.Var = factor(cat.Var,
+                       levels = unique(cat.Var))
+    ) %>%
+      # \___Sort by Group and Category ----
+    arrange(x.axis.Var, desc(cat.Var)) %>%
+      # \___Get the start and end points of the bars ----
+    mutate(end.Bar = cumsum(values),
+           start.Bar = c(0, head(end.Bar, -1))) %>%
+      # \___Put variables in the same order ----
+    select(names(df),end.Bar,start.Bar)
+  ) %>%
+    # \_Get numeric index for the groups ----
+  mutate(group.id = group_indices(., x.axis.Var)) %>%
+    # \_Create new variable with total by group ----
+  group_by(x.axis.Var) %>%
+    mutate(total.by.x = sum(values)) %>%
+    # \_Order the columns ----
+  select(x.axis.Var, cat.Var, group.id, start.Bar, values, end.Bar, total.by.x)
+
+  ggplot(df.tmp, aes(x = group.id, fill = cat.Var)) +
+    # \_Simple Waterfall Chart ----
+  geom_rect(aes(x = group.id,
+                xmin = group.id - 0.5, # control bar gap width
+                xmax = group.id + 0.5,
+                ymin = end.Bar,
+                ymax = start.Bar),
+            color="black",
+            alpha=0.95) +
+    # \_Lines Between Bars ----
+
+  # \_Numbers inside bars (each category) ----
+  geom_text(
+    mapping =
+      aes(
+        label = ifelse(values > threshold,human_numbers(values),""),
+        y = rowSums(cbind(start.Bar,values/2))
+      ),
+    color = "white",
+    fontface = "bold"
+  ) +
+    # \_Total for each category above bars ----
+  geom_text(
+    mapping =
+      aes(
+        label = ifelse(cat.Var != tKey,
+                       "",
+                       human_numbers(total.by.x)
+        ),
+        y = end.Bar+threshold
+      ),
+    color = "#4e4d47",
+    fontface = "bold"
+  ) +
+    # \_Change colors ----
+  scale_fill_manual(values=nhmDefaultColors()) +
+    # \_Change y axis to same scale as original ----
+
+  # \_Add tick marks on x axis to look like the original plot ----
+  scale_x_continuous(
+    expand=c(0,0),
+    limits = c(min(df.tmp$group.id)-0.5,max(df.tmp$group.id)+0.5),
+    breaks = c(min(df.tmp$group.id)-0.5,
+               unique(df.tmp$group.id),
+               unique(df.tmp$group.id) + 0.5
+    ),
+    labels =
+      c("",
+        as.character(unique(df.tmp$x.axis.Var)),
+        rep(c(""), length(unique(df.tmp$x.axis.Var)))
+      )
+  ) +
+    # \_Theme options to make it look like the original plot ----
+  theme(
+    text = element_text(size = 14, color = "#4e4d47"),
+    axis.text = element_text(angle=25,size = 10, color = "#4e4d47", face = "bold", vjust=0.5, hjust=1),
+    #axis.text.y = element_text(margin = margin(r = 0.3, unit = "cm")),
+    axis.ticks.x =
+      element_line(color =
+                     c("black",
+                       rep(NA, length(unique(df.tmp$x.axis.Var))),
+                       rep("black", length(unique(df.tmp$x.axis.Var))-1)
+                     )
+      ),
+    axis.line = element_line(colour = "#4e4d47", size = 0.5),
+    axis.ticks.length = unit(.15, "cm"),
+    axis.title.x =       element_blank(),
+    panel.background =   element_blank(),
+    plot.margin =        unit(c(1, 1, 1, 1), "lines"),
+    legend.text =        element_text(size = 10,
+                                      color = "#4e4d47",
+                                      face = "bold",
+                                      margin = margin(l = 0.25, unit = "cm")
+    ),
+    legend.title =       element_blank()
+  ) +
+    scale_y_continuous(labels = human_numbers) +
+    labs(x="xlabel", y=yTitle, title=graphTitle)
+
+}
